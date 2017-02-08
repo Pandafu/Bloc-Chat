06b8f89f823ff03c561a9b59ffbc499f8f9199b3
Pandafu <developermode@Risss-MacBook-Pro.local>
2017-02-08 10:21:35 -0600

    trouble with displaying active room

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,28 @@
 (function(){
-    function HomeCtrl(Room){
+    function HomeCtrl($scope, Message, Room){
         //passsing Room to the HomeCtrl
-        this.rooms = Room.all; 
-
+        //this.rooms = Room.all; 
+        var rooms = Room.all;
+        rooms.$loaded().then(function(rooms){
+            //declare a key variable, set it equal to the roomId
+            var activeroomId = "-Kbmjwa5OAYl1j7eXFbF";
+            //create a room by querying rooms with the roomId
+            var room = rooms.$getRecord(activeroomId);
+            $scope.activeRoom = room;
+            $scope.messages = Message.getByRoomId(activeroomId);
+        });
+        //makes it a property on HomeCtrl
+        //also makes it accessible to instances of the controller
+        this.rooms = rooms;
+        
+        //this method changes between chat rooms
+        this.selectroom = function(room){
+            $scope.activeRoom = room;
+            $scope.messages = Message.getByRoomId($scope.activeRoom.$id);
+        }
     }
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ["Room", HomeCtrl]);
+        .controller('HomeCtrl', ["$scope", "Message", "Room", HomeCtrl]);
 })();
+
--- app/scripts/controllers/MessageCtrl.js
+++ app/scripts/controllers/MessageCtrl.js
@@ -3,20 +3,7 @@
         var channelsCtrl = this;
         channelsCtrl.channels = channels;
         
-        /*
-        $scope.activeRoom = {};
-        $scope.selectRoom = [];
-        
-        $scope.selectRoom = function(Message){
-            ref.
-        };
-        
-        $scope.activeRoom = function(){
-           ref.orderByChild("roomId").on("messages", function(snapshot){
-               return snapshot;
-           })
-        };
-        */ 
+ 
     };
     angular
         .module('blocChat')
