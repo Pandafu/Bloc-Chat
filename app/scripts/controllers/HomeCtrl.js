4f2d1a955ce83b6ffc0850b2bd389510338726ed
Riss <rissgadi@gmail.com>
2017-02-13 19:22:05 -0600

    completed checkpoint 4

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -5,7 +5,7 @@
         var rooms = Room.all;
         rooms.$loaded().then(function(rooms){
             //declare a key variable, set it equal to the roomId
-            var activeroomId = "-Kbmjwa5OAYl1j7eXFbF";
+            var activeroomId = " ";
             //create a room by querying rooms with the roomId
             var room = rooms.$getRecord(activeroomId);
             $scope.activeRoom = room;
@@ -14,13 +14,15 @@
         //makes it a property on HomeCtrl
         //also makes it accessible to instances of the controller
         this.rooms = rooms;
-        
         //this method changes between chat rooms
         this.selectroom = function(room){
             $scope.activeRoom = room;
             $scope.messages = Message.getByRoomId($scope.activeRoom.$id);
+
         }
+
     }
+
     angular
         .module('blocChat')
         .controller('HomeCtrl', ["$scope", "Message", "Room", HomeCtrl]);
